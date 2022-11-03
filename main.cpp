f476a0b9c81af6734b2a3b95504133a05b9b17e1
Youssef Bakouny <Youssef.Bakouny@usj.edu.lb>
2022-11-03 16:12:41 +0200

    Implement methods

--- main.cpp
+++ main.cpp
@@ -1,17 +1,32 @@
 #include <iostream>
-
+#include <sstream>
 using namespace std;
 
 class MyClass{
   public:
    
-   
+   string to_string() const;
    MyClass operator+(const MyClass& c) const;
      
    
   private:
    int x;
 };
-ostream& operator<<(ostream& os, const MyClass& mc);
+
+
+string MyClass::to_string() const {
+  stringstream ss;
+  ss << "MyClass: " << x << endl;
+  return ss.str(); 
+}
+
+MyClass MyClass::operator+(const MyClass& c) const {
+  return MyClass(x+c.x);
+}
+
+ostream& operator<<(ostream& os, const MyClass& mc) {
+  os << mc.to_string();
+  return os;
+}
 
 
